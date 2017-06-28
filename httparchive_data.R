e9b3365172059801001bb013d7e15655a428d73a
Michal Ziembinski <m.ziembinski@gmail.com>
2017-06-28 14:54:20 +0100

    httparchive bytes types and comments

--- httparchive_data.R
+++ httparchive_data.R
@@ -14,11 +14,14 @@ library(ggplot2)
 # Use your project ID here
 project <- "httparchiveproject" # put your project ID here
 
-dates<-c(paste0(2012:2017,"_01_01"),paste0(2012:2017,"_04_01"),paste0(2012:2016,"_07_01"),paste0(2012:2016,"_10_01"))
+# Dates vector (there is no bytes data for 2017-01-01, so we add 2017-02-15 to the set)
+dates<-c(paste0(2012:2017,"_01_01"),paste0(2012:2017,"_04_01"),paste0(2012:2016,"_07_01"),paste0(2012:2016,"_10_01"),"2017_02_15")
+dates<-dates[order(dates)]
 
-
+# loop through dates and download data from google bigquery service
+# for more expl. see: https://www.igvita.com/2013/06/20/http-archive-bigquery-web-performance-answers/
 for(i in 1:length(dates)){
-  sql <- paste0("SELECT pageid, url, rank, onLoad, fullyLoaded, reqTotal, reqHtml, reqJS, bytesTotal, bytesHtml, bytesJS FROM [httparchive:runs.",dates[i],"_pages] 
+  sql <- paste0("SELECT pageid, url, rank, onLoad, fullyLoaded, reqTotal, reqHtml, reqJS, bytesTotal, bytesHtml, bytesJS,bytesCSS, bytesIPic=bytesImg+bytesGif+bytesJpg+bytesPng, bytesFont, bytesJson FROM [httparchive:runs.",dates[i],"_pages] 
         WHERE rank IS NOT NULL and rank<10000
               ORDER BY rank asc")
 
@@ -27,17 +30,18 @@ for(i in 1:length(dates)){
   tmp<-data.table(tmp)
   tmp[,date:=dates[i]]
   
-#  tmp[,jsI:=1]
-#  tmp[,jsI:=as.numeric(cumsum(jsI)),by=pageid]
-
+# combine results
   ifelse(i==1,dataBQ<-tmp,dataBQ<-rbind(dataBQ,tmp))
   print(i)
 }
 
+# check the results
 dataBQ[,.N,by=date][order(date)]
 
+# save the downloaded data 
 save(dataBQ,file="data/dataBQ.RData")
 
+#### plotting to check/explore data ####
 ggplot(dataBQ[order(date)],aes(date, bytesJS/bytesTotal))+geom_boxplot()
 
 ggplot(dataBQ[,.(bytesJS=mean(bytesJS,na.rm=T)),by=date][order(date)][date!="2017_01_01"],aes(date, bytesJS,group=1))+geom_line()+theme_bw()+
